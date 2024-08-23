7d40c75aa76dae086f305e463d8505e0d522ac03
teazaty <162536388+teazaty@users.noreply.github.com>
2024-08-23 17:48:35 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -99,10 +99,7 @@ class HashCats:
                 except Exception as e:
                     base.log(f"{base.red}Error: {base.white}{e}")
 
-            print()
-            wait_time = 30 * 60
-            base.log(f"{base.yellow}Wait for {int(wait_time/60)} minutes!")
-            time.sleep(wait_time)
+            break
 
 
 if __name__ == "__main__":
